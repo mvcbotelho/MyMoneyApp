f84e400c1ad734580c2d9b51e16a7c85adb473ef
mvcbotelho <marcus.itec@gmail.com>
2018-03-02 09:50:35 -0300

    Change in BillingCycle

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -13,20 +13,21 @@ BillingCycle.route('count', (req, res, next) => {
   })
 })
 
-BillingCycle.route('summary', (req, res, next) => {
+BillingCycle.route('summary', (req, res, next) =>{
   BillingCycle.aggregate({
-    $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" }}
-    }, {
-    $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" }}
-    }, {
-    $project: { _id: 0, credit: 1, debt: 1 }
-    }, (error, result) => {
-      console.log('RESULT - ', result)
-      if (error) {
-        res.status(500).json({ errors: [error] })
-      } else {
-        res.json(result[0] || { credit: 0, debt: 0 })
-      }
+    $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+  }, {
+    $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+  }, {
+    $project: {_id: 0, credit: 1, debt: 1}
+  }, (error, result) => {
+    console.log('result', result)
+    //console.log('result', result[0])
+    if(error) {
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json(result || {credit: 0, debt: 0})
+    }
   })
 })
 
