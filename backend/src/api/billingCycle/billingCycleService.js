935256528efcca4d9f3bd0902873b3bfba6c4a51
mvcbotelho <marcus.itec@gmail.com>
2018-03-12 17:40:28 -0300

    add errorHandle

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,9 @@
 const BillingCycle = require('./billingCycle')
+const errorHandle = require('../common/errorHandle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.after('post', errorHandle).after('put', errorHandle)
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
@@ -12,22 +14,6 @@ BillingCycle.route('count', (req, res, next) => {
         }
     })
 })
-
-// BillingCycle.route('summary', (req, res, next) => {
-//     BillingCycle.aggregate({
-//         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
-//     }, {
-//         $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-//     }, {
-//         $project: {_id: 0, credit: 1, debt: 1}
-//     }, (error, result) => {
-//         if(error) {
-//             res.status(500).json({errors: [error]})
-//         } else {
-//             res.json(result[0] || { credit: 0, debt: 0 })
-//         }
-//     })
-// })
 
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate([{
