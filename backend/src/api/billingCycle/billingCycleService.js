1860921acf1fcf7f281900a69f84c7a422f019eb
mvcbotelho <marcus.itec@gmail.com>
2018-03-12 17:22:57 -0300

    Corrections in moongose

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -13,14 +13,30 @@ BillingCycle.route('count', (req, res, next) => {
     })
 })
 
+// BillingCycle.route('summary', (req, res, next) => {
+//     BillingCycle.aggregate({
+//         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
+//     }, {
+//         $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+//     }, {
+//         $project: {_id: 0, credit: 1, debt: 1}
+//     }, (error, result) => {
+//         if(error) {
+//             res.status(500).json({errors: [error]})
+//         } else {
+//             res.json(result[0] || { credit: 0, debt: 0 })
+//         }
+//     })
+// })
+
 BillingCycle.route('summary', (req, res, next) => {
-    BillingCycle.aggregate({
+    BillingCycle.aggregate([{
         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
     }, {
         $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
     }, {
         $project: {_id: 0, credit: 1, debt: 1}
-    }, (error, result) => {
+    }]).exec((error, result) => {
         if(error) {
             res.status(500).json({errors: [error]})
         } else {
