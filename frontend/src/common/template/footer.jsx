68c12348eaa5592c6595492dc1c6ff0856654708
mvcbotelho <marcus.itec@gmail.com>
2018-03-13 16:58:18 -0300

    Create footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2018
+      <a href="http://octopustecnologia.com.br" target='_blank'> Octopus Tecnologia</a>
+    </strong>
+  </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,13 @@ import '../common/template/dependencies'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
   <div className='wrapper'>
     <Header />
     <SideBar />
+    <div className='content-wrapper'>App</div>
+    <Footer />
   </div>
 )
