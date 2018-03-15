3357358efd2e154de3ec8544f4b66e4c0dbbb249
mvcbotelho <marcus.itec@gmail.com>
2018-03-15 19:50:30 -0300

    Add content component in app

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Content from '../common/template/content'
+import ContentHeader from '../common/template/contentHeader'
 
-export default props => (
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-)
+class Dashboard extends Component {
+  render () {
+    return (
+      <div>
+        <ContentHeader title='Dashboard' small='V1.0' />
+        <Content>Dashboard</Content>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
