ee55f12777dc308decef53ebc6e870c60189e65b
mvcbotelho <marcus.itec@gmail.com>
2018-03-15 19:39:03 -0300

    Add routes from main page and billin cycles

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,12 +4,15 @@ import '../common/template/dependencies'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
+import Routes from './routes'
 
 export default props => (
   <div className='wrapper'>
     <Header />
     <SideBar />
-    <div className='content-wrapper'>App</div>
+    <div className='content-wrapper'>
+      <Routes />
+    </div>
     <Footer />
   </div>
 )
