8d4bbef7028d0e9827f4208bd61c4f35618bc124
mvcbotelho <marcus.itec@gmail.com>
2018-03-16 14:02:40 -0300

    Add middleware and reducers in project

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
+import { getSummary } from './dashboardActions'
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
 
@@ -8,6 +10,10 @@ import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+  componentWillMount () {
+    this.props.getSummary()
+  }
+  
   render () {
     const { credit, debt } = this.props.summary
     return (
@@ -44,5 +50,5 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({summary: state.dashboard.summary})
-
-export default connect(mapStateToProps)(Dashboard)
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
