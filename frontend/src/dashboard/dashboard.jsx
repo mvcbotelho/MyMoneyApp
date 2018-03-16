7eabcd2f0c1c2152ddd70af547e16ceff4fbec8f
mvcbotelho <marcus.itec@gmail.com>
2018-03-16 08:11:22 -0300

    Add redux in app

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
@@ -8,6 +9,7 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
   render () {
+    const { credit, debt } = this.props.summary
     return (
       <div>
         <ContentHeader title='Dashboard' small='V1.0' />
@@ -17,21 +19,21 @@ class Dashboard extends Component {
               cols='12 4'
               color='green'
               icon='bank'
-              value='R$10,00'
+              value={`R$ ${credit}`}
               text='Total de crédito'
             />
             <ValueBox
               cols='12 4'
               color='red'
               icon='credit-card'
-              value='R$10,00'
+              value={`R$ ${debt}`}
               text='Total de débitos'
             />
             <ValueBox
               cols='12 4'
               color='blue'
               icon='money'
-              value='R$10,00'
+              value={`R$ ${credit - debt}`}
               text='Valor consolidado'
             />
           </Row>
@@ -41,4 +43,6 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+
+export default connect(mapStateToProps)(Dashboard)
