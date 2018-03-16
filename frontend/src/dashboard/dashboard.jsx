4f836ab4d8287ecaeb0c632e45a42c67e0c42965
mvcbotelho <marcus.itec@gmail.com>
2018-03-16 07:51:03 -0300

    Add value box, grid and row in template

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
+
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
   render () {
     return (
       <div>
         <ContentHeader title='Dashboard' small='V1.0' />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              cols='12 4'
+              color='green'
+              icon='bank'
+              value='R$10,00'
+              text='Total de crédito'
+            />
+            <ValueBox
+              cols='12 4'
+              color='red'
+              icon='credit-card'
+              value='R$10,00'
+              text='Total de débitos'
+            />
+            <ValueBox
+              cols='12 4'
+              color='blue'
+              icon='money'
+              value='R$10,00'
+              text='Valor consolidado'
+            />
+          </Row>
+        </Content>
       </div>
     )
   }
