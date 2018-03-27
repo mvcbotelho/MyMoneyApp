d8ed5c7d60122d03b768bdc268d2ba86c55e8cea
mvcbotelho <marcus.itec@gmail.com>
2018-03-26 21:53:39 -0300

    Add credit list in project

--- frontend/src/billingCycle/billingCycleForms.jsx
+++ frontend/src/billingCycle/billingCycleForms.jsx
@@ -5,6 +5,7 @@ import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import labelAndInput from '../common/form/labelAndInput'
+import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
   render () {
@@ -19,6 +20,7 @@ class BillingCycleForm extends Component {
             label='Mês' cols='12 4' placeholder='Informe o mês'/>
           <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
             label='Ano' cols='12 4' placeholder='Informe o ano'/>
+          <CreditList cols='12 6' />
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
