d67847a6ac035fffbd49fac7d5985aa942592267
mvcbotelho <marcus.itec@gmail.com>
2018-03-27 21:16:54 -0300

    add itemList for create list credits and debts

--- frontend/src/billingCycle/billingCycleForms.jsx
+++ frontend/src/billingCycle/billingCycleForms.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
   render () {
@@ -20,7 +20,7 @@ class BillingCycleForm extends Component {
             label='Mês' cols='12 4' placeholder='Informe o mês'/>
           <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
             label='Ano' cols='12 4' placeholder='Informe o ano'/>
-          <CreditList cols='12 6' list={credits} readOnly={readOnly} />
+          <ItemList cols='12 6' list={credits} readOnly={readOnly} />
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
