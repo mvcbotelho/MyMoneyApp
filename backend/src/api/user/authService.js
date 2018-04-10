edaee4a8511903042e8fbe66ea62a3c55c6ea716
mvcbotelho <marcus.itec@gmail.com>
2018-04-09 22:26:23 -0300

    Add message error from db

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -6,3 +6,10 @@ const env = require('../../.env')
 
 const emailRegex = /\S+@\S+\.S+/
 const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/
+
+const sendErrorsFromDB = (res, dbErrors) => {
+  const errors = []
+  _.forIn(dbErrors.errors, error => errors.push(error.message))
+
+  returnres.status(400).json({ errors })
+}
